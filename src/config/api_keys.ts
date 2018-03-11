69c6cd62f1deddac04ad5da16d5b34d72b9a0eff
harpreetsingh91 <harpreet.singhgt@gmail.com>
2018-03-11 01:20:58 -0800

    hack for heroku :X

--- src/config/api_keys.ts
+++ src/config/api_keys.ts
@@ -1,6 +1,8 @@
   export const amazonKey = {
-    accessKeyId: process.env.awsAccessKey,
-    secretAccessKey: process.env.awsSecret,
+    // accessKeyId: process.env.awsAccessKey,
+    // secretAccessKey: process.env.awsSecret,
+    accessKeyId: "AKIAIGEF2E7IKY5TDL4A",
+    secretAccessKey: "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",
     associateTag: "harpreetvic-20",
     locale: "webservices.amazon.ca"
   };
