86c13361151feb88382e7541ed621e6a5b5a82c5
harpreetsingh91 <harpreet.singhgt@gmail.com>
2018-03-11 00:18:19 -0800

    Removed mongo

--- dist/app.js
+++ dist/app.js
@@ -17,33 +17,16 @@ const body_parser_1 = __importDefault(require("body-parser"));
 const morgan_1 = __importDefault(require("morgan"));
 const lusca_1 = __importDefault(require("lusca"));
 const dotenv_1 = __importDefault(require("dotenv"));
-const connect_mongo_1 = __importDefault(require("connect-mongo"));
 const express_flash_1 = __importDefault(require("express-flash"));
 const path_1 = __importDefault(require("path"));
-const mongoose_1 = __importDefault(require("mongoose"));
 const passport_1 = __importDefault(require("passport"));
 const express_validator_1 = __importDefault(require("express-validator"));
-const bluebird_1 = __importDefault(require("bluebird"));
-const MongoStore = connect_mongo_1.default(express_session_1.default);
 // Load environment variables from .env file, where API keys and passwords are configured
 dotenv_1.default.config({ path: ".env.example" });
 // Controllers (route handlers)
-const homeController = __importStar(require("./controllers/home"));
-const userController = __importStar(require("./controllers/user"));
-const apiController = __importStar(require("./controllers/api"));
-const contactController = __importStar(require("./controllers/contact"));
 const daanApiController = __importStar(require("./controllers/daan"));
-// API keys and Passport configuration
-const passportConfig = __importStar(require("./config/passport"));
 // Create Express server
 const app = express_1.default();
-// Connect to MongoDB
-const mongoUrl = process.env.MONGOLAB_URI;
-mongoose_1.default.Promise = bluebird_1.default;
-mongoose_1.default.connect(mongoUrl, { useMongoClient: true }).then(() => { }).catch(err => {
-    console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
-    // process.exit();
-});
 // Express configuration
 app.set("port", process.env.PORT || 3000);
 app.set("views", path_1.default.join(__dirname, "../views"));
@@ -57,10 +40,6 @@ app.use(express_session_1.default({
     resave: true,
     saveUninitialized: true,
     secret: process.env.SESSION_SECRET,
-    store: new MongoStore({
-        url: mongoUrl,
-        autoReconnect: true
-    })
 }));
 app.use(passport_1.default.initialize());
 app.use(passport_1.default.session());
@@ -90,35 +69,12 @@ app.use(express_1.default.static(path_1.default.join(__dirname, "public"), { max
 /**
  * Primary app routes.
  */
-app.get("/", homeController.index);
-app.get("/login", userController.getLogin);
-app.post("/login", userController.postLogin);
-app.get("/logout", userController.logout);
-app.get("/forgot", userController.getForgot);
-app.post("/forgot", userController.postForgot);
-app.get("/reset/:token", userController.getReset);
-app.post("/reset/:token", userController.postReset);
-app.get("/signup", userController.getSignup);
-app.post("/signup", userController.postSignup);
-app.get("/contact", contactController.getContact);
-app.post("/contact", contactController.postContact);
-app.get("/account", passportConfig.isAuthenticated, userController.getAccount);
-app.post("/account/profile", passportConfig.isAuthenticated, userController.postUpdateProfile);
-app.post("/account/password", passportConfig.isAuthenticated, userController.postUpdatePassword);
-app.post("/account/delete", passportConfig.isAuthenticated, userController.postDeleteAccount);
-app.get("/account/unlink/:provider", passportConfig.isAuthenticated, userController.getOauthUnlink);
 /**
  * API examples routes.
  */
-app.get("/api", apiController.getApi);
-app.get("/api/facebook", passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.getFacebook);
 /**
  * OAuth authentication routes. (Sign in)
  */
-app.get("/auth/facebook", passport_1.default.authenticate("facebook", { scope: ["email", "public_profile"] }));
-app.get("/auth/facebook/callback", passport_1.default.authenticate("facebook", { failureRedirect: "/login" }), (req, res) => {
-    res.redirect(req.session.returnTo || "/");
-});
 /**
  * Daan API routes
  */
